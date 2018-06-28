bb1402f4ac3271e30f251355c880902033d6c693
Andela Developer <tosmak16@gmail.com>
2018-06-28 16:56:04 +0100

    add add category model and Ingredient model

--- ingredients/models.py
+++ ingredients/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name='ingredients', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
