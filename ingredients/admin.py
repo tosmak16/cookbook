b8bc82498b0e1bc5a0dfebc4d3cc5cb7c70dc392
Andela Developer <tosmak16@gmail.com>
2018-06-29 16:04:16 +0100

    add ingredients seeder data

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from ingredients.models import Category, Ingredient
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Ingredient)
